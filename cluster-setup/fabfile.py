a49c4e17fb74323fb1d40922596eb1171edb1878
mzasada <maciejekz@gmail.com>
2014-10-22 21:50:19 +1100

    Add provisioning of Solr 4.6 cluster
    Added recipe for provisioning Solr cloud cluster with 2 nodes +
    embedded ZooKeeper.

--- cluster-setup/fabfile.py
+++ cluster-setup/fabfile.py
@@ -1,20 +1,51 @@
+# coding=utf-8
 from fabric.api import task
+from fabric.context_managers import cd
+from fabric.operations import run
 
 from fabtools import require
 from fabtools.require import deb
 
+LOCATION_SOLR_4_6_0 = 'http://archive.apache.org/dist/lucene/solr/4.6.0/solr-4.6.0.tgz'
+
 
 @task
 def provision():
-    install_jdk7()
-    install_solr(port=9001)
-    install_solr(port=9002, zk_host='localhost:9001')
+    install_dependencies()
+    setup_cloud()
 
 
-def install_solr(port, war_location='2.6.4', where='', zk_host=None, jetty_location=''):
-    if zk_host is not None:
-        print('')
-
-
-def install_jdk7():
+def install_dependencies():
     require.deb.package('openjdk-7-jre-headless', update=True)
+    require.deb.package('dtach', update=True)
+
+
+def setup_cloud():
+    with cd('~/'):
+        # run('wget –q {0}'.format(LOCATION_SOLR_4_6_0))
+        run('tar xf solr-4.6.0.tgz solr-4.6.0')
+
+    with cd('~/solr-4.6.0'):
+        run('cp -r example node-1')
+        run('cp -r example node-2')
+
+    with cd('~/solr-4.6.0/node-1'):
+        run_in_bg('java '
+                  '-Djetty.port=9001 '
+                  '-DzkRun '
+                  '-DnumShards=2 '
+                  '-Dbootstrap_confdir=./solr/collection1/conf '
+                  '-Dcollection.configName=myconf '
+                  '-jar start.jar '
+                  '>& /dev/null < /dev/null')
+
+    with cd('~/solr-4.6.0/node-2'):
+        run_in_bg(' java '
+                  '-Djetty.port=9002 '
+                  '-DzkHost=localhost:10001 '
+                  '-jar start.jar '
+                  '>& /dev/null < /dev/null')
+
+
+def run_in_bg(cmd, sockname='dtach'):
+    run("dtach -n `mktemp -u /tmp/{}.XXXX` {}".format(sockname, cmd))
