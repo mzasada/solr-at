bd1250c0095232aef85ea54fe79ae35e4d065be4
mzasada <maciejekz@gmail.com>
2014-10-23 14:23:47 +1100

    Add parametrizied AT

--- cluster-tests/src/test/groovy/org/apache/solr/DistributedDeadlockTest.groovy
+++ cluster-tests/src/test/groovy/org/apache/solr/DistributedDeadlockTest.groovy
@@ -1,38 +1,58 @@
 package org.apache.solr
 
 import org.apache.solr.client.solrj.SolrQuery
-import org.apache.solr.client.solrj.impl.CloudSolrServer
+import org.apache.solr.client.solrj.SolrServer
+import org.apache.solr.client.solrj.impl.LBHttpSolrServer
+import org.apache.solr.client.solrj.response.QueryResponse
+import org.apache.solr.common.SolrInputDocument
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.util.concurrent.PollingConditions
 
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 class DistributedDeadlockTest extends Specification {
 
-  private static final int REQUEST_COUNT = 200
+  private static final int REQUEST_COUNT = 1
 
-  CloudSolrServer server = new CloudSolrServer("localhost:10001").with {
-    it.setDefaultCollection("collection1")
-    it
-  }
+  @Shared
+  def connections = connectToClusters()
 
   def executor = Executors.newFixedThreadPool(20)
 
   def cleanup() {
     executor.shutdown()
-    server.shutdown()
+    connections.each {
+      it.deleteByQuery("*:*")
+      it.commit()
+      it.shutdown()
+    }
   }
 
-  def "should fail"() {
+  def setup() {
+    connections.each {
+      it.add(
+          new SolrInputDocument().with {
+            it.addField("id", "001")
+            it.addField("name", "awesome document")
+            it
+          }
+      )
+      it.commit()
+    }
+  }
+
+  def "should not detect any deadlocks"() {
     given:
     def conditions = new PollingConditions(timeout: 5, initialDelay: 1, delay: 0.5)
-    def responses = []
+    List<Future<QueryResponse>> responses = []
 
     when:
     (1..REQUEST_COUNT).each {
       responses << executor.submit({
-        server.query(new SolrQuery("*:*"))
+        conection.query(new SolrQuery("*:*"))
       } as Callable)
     }
 
@@ -40,8 +60,19 @@ class DistributedDeadlockTest extends Specification {
     conditions.eventually {
       assert responses.size() == REQUEST_COUNT
       assert responses.every {
-        it.get() != null
+        it.get().getResults().size() == 1
       }
     }
+
+    where:
+    conection << connections
+  }
+
+  private List<SolrServer> connectToClusters() {
+    [
+        // vanilla Apache Solr 4.6.0
+        new LBHttpSolrServer("http://localhost:9001/solr/collection1", "http://localhost:9002/solr/collection1")
+        // Modified Apache Solr 4.6.0
+    ]
   }
 }
