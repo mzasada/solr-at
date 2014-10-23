40ffaf051a60c3161734c1f5f159dfe0e8eab335
mzasada <maciejekz@gmail.com>
2014-10-23 15:57:15 +1100

    Automated Solr cluster restart
    Invoking ‘fab vagrant restart_clusters’ will remove current clusters
    and start new ones from scratch.

--- cluster-setup/fabfile.py
+++ cluster-setup/fabfile.py
@@ -2,17 +2,25 @@
 from fabric.api import task
 from fabric.context_managers import cd
 from fabric.operations import run
+from fabtools.vagrant import vagrant
 
 from fabtools import require, oracle_jdk
-from fabtools.require import deb
 
-LOCATION_SOLR_4_6_0 = 'http://archive.apache.org/dist/lucene/solr/4.6.0/solr-4.6.0.tgz'
+SOLR_4_6_0_DOWNLOAD_URL = 'http://archive.apache.org/dist/lucene/solr/4.6.0/solr-4.6.0.tgz'
+SOLR_4_6_0_LOCATION = "~/solr-4.6.0"
 
 
 @task
 def provision():
     install_dependencies()
-    setup_cloud()
+    download_solr_distro()
+    bootstrap_cluster()
+
+
+@task
+def restart_clusters():
+    run('killall -9 java || exit 0')
+    bootstrap_cluster()
 
 
 def install_dependencies():
@@ -20,33 +28,37 @@ def install_dependencies():
     require.deb.package('dtach', update=True)
 
 
-def setup_cloud():
+def download_solr_distro():
     with cd('~/'):
-        run('wget -o solr-download.log –q {0} || exit 0'.format(LOCATION_SOLR_4_6_0))
-        run('tar xf solr-4.6.0.tgz solr-4.6.0')
+        run('wget -o solr-download.log –q {0} || exit 0'.format(SOLR_4_6_0_DOWNLOAD_URL))
+        run('tar xf solr-4.6.0.tgz {0}'.format(SOLR_4_6_0_LOCATION))
 
-    with cd('~/solr-4.6.0'):
+
+def bootstrap_cluster():
+    with cd(SOLR_4_6_0_LOCATION):
+        run('rm -rf node-1')
+        run('rm -rf node-2')
         run('cp -r example node-1')
         run('cp -r example node-2')
 
-    with cd('~/solr-4.6.0/node-1'):
-        run('cp /vagrant/jetty/jetty.xml etc/jetty.xml')
-        run_in_bg('java '
-                  '-Djetty.port=9001 '
-                  '-DzkRun '
-                  '-DnumShards=2 '
-                  '-Dbootstrap_confdir=./solr/collection1/conf '
-                  '-Dcollection.configName=myconf '
-                  '-jar start.jar '
-                  '>& /dev/null < /dev/null')
+        with cd('node-1'):
+            run('cp /vagrant/jetty/jetty.xml etc/jetty.xml')
+            run_in_bg('java '
+                      '-Djetty.port=9001 '
+                      '-DzkRun '
+                      '-DnumShards=2 '
+                      '-Dbootstrap_confdir=./solr/collection1/conf '
+                      '-Dcollection.configName=myconf '
+                      '-jar start.jar '
+                      '>& /dev/null < /dev/null')
 
-    with cd('~/solr-4.6.0/node-2'):
-        run('cp /vagrant/jetty/jetty.xml etc/jetty.xml')
-        run_in_bg('java '
-                  '-Djetty.port=9002 '
-                  '-DzkHost=localhost:10001 '
-                  '-jar start.jar '
-                  '>& /dev/null < /dev/null')
+        with cd('node-2'):
+            run('cp /vagrant/jetty/jetty.xml etc/jetty.xml')
+            run_in_bg('java '
+                      '-Djetty.port=9002 '
+                      '-DzkHost=localhost:10001 '
+                      '-jar start.jar '
+                      '>& /dev/null < /dev/null')
 
 
 def run_in_bg(cmd, sockname='dtach'):
